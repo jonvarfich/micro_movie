acb446cdfb3db03c1d0bb1bd2c02de1beafd0ba8
jonvarfich <jonvarfich@gmail.com>
2021-03-05 11:58:23 -0400

    home view progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchbarComponent } from './Components/searchbar/searchbar.component';
+import { HeaderComponent } from './Components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent,
+    SearchbarComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
