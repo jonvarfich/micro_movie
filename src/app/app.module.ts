625f6fbcefcfd5c387eaee6d27cf63c4ea38528a
Khan997 <thexapholox@gmail.com>
2021-03-05 14:50:43 -0600

    otro mas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
-import { LoginComponent } from './views/login/login.component';
-import { NavbarComponent } from './Components/navbar/navbar.component';
-import { SearchbarComponent } from './Components/searchbar/searchbar.component';
-import { HeaderComponent } from './Components/header/header.component';
+import { LoginComponent } from './auth/login/login.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    NavbarComponent,
-    SearchbarComponent,
-    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
