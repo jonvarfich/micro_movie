949fa18fc6351a67a2d905fbcc34793595db4dda
jonvarfich <jonvarfich@gmail.com>
2021-03-05 12:14:02 -0400

    home view progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 import { SearchbarComponent } from './Components/searchbar/searchbar.component';
 import { HeaderComponent } from './Components/header/header.component';
 
