d5357672e1d035a8427a9c944da906e653655bfb
jonvarfich <jonvarfich@gmail.com>
2021-03-05 19:38:31 -0400

    halp not needed anymore

--- micromovie/src/app/app.module.ts
+++ micromovie/src/app/app.module.ts
@@ -6,13 +6,20 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './auth/home/home.component';
 import { NavbarComponent } from './Component/navbar/navbar.component';
 import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { MoviesComponent } from './auth/movies/movies.component';
+//////////////////
+
+//////////////////
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
---
+++ micromovie/src/app/auth/movies/movies.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeRoutingModule } from './movies-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class MoviesModule { }
